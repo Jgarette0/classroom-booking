ac1ffa714bf230885f1adc1ed00b81432d7d7301
Jilian Garette Abangan <92016876+Jgarette0@users.noreply.github.com>
2023-11-16 23:09:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>CLASSROOM INTEGRATION SYSTEM</title>
+      <title>CLASSROOM</title>
       <link rel="stylesheet" href="accounts/user.css">
       <link rel="stylesheet" href="css/responsive.css">
     </head>
